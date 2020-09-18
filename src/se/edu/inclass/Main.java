c1f4b9270b55f39b3859a036499ea57ee1f72dbf
harryleecp <e0406315@u.nus.edu>
2020-09-18 17:45:53 +0800

    Added filterbystring method

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -20,7 +20,9 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        printDeadlinesUsingStreams(tasksData);
 
+        System.out.println(filterByString(tasksData, "11"));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -37,6 +39,21 @@ public class Main {
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        tasksData.stream()
+                .filter((s) -> s instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasksData, String filterString) {
+        ArrayList<Task> filteredTaskList;
+        filteredTaskList = (ArrayList<Task>) tasksData.stream()
+                .filter((s) -> s.getDescription().contains(filterString))
+                .collect(Collectors.toList());
+        return filteredTaskList;
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
