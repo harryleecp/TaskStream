c922c01c9d84a5e8c8d3d7eb4e0aff20694b8d29
harryleecp <e0406315@u.nus.edu>
2020-09-18 17:21:59 +0800

    Added commands in main function

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,11 +16,15 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
+
         System.out.println("Printing deadlines");
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDataUsingStreams(tasksData);
+        printDeadlinesUsingStreams(tasksData);
+        System.out.println(getNumberOfDeadlines(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
